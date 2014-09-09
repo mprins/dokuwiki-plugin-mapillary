9d8d37e6fc992f737ccad0222f05277560f15779
Mark C. Prins <mprins@users.sf.net>
2014-09-09 15:14:38 +0000

    use a figure instead of a div for the print alternative

--- syntax.php
+++ syntax.php
@@ -38,7 +38,7 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 	public function getType() {
 		return 'substition';
 	}
-	
+
 	/**
 	 *
 	 * @see DokuWiki_Syntax_Plugin::getPType()
@@ -46,7 +46,7 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 	public function getPType() {
 		return 'block';
 	}
-	
+
 	/**
 	 *
 	 * @see Doku_Parser_Mode::getSort()
@@ -54,13 +54,14 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 	public function getSort() {
 		return 305;
 	}
-	
+
 	/**
 	 * Define the syntax pattern.
 	 * The syntax for this plugin is: {{mapillary>imagehash&width&sequences&legs}}
 	 * where imagehash is the hash of the first image of a sequence
-	 * and width is the widget width in pixels. One (optional) space either after the opening pair 
-	 * of brackets or before the closing pair signifies floating aligment to right of left.
+	 * and width is the widget width in pixels. One (optional) space
+	 * either after the opening pair of brackets or before the closing
+	 * pair signifies floating aligment to right of left.
 	 *
 	 * @see http://www.mapillary.com/integrate.html
 	 *
@@ -69,7 +70,7 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 	public function connectTo($mode) {
 		$this->Lexer->addSpecialPattern ( '\{\{\s?mapillary>[^}\s]*\s?\}\}', $mode, 'plugin_mapillary' );
 	}
-	
+
 	/**
 	 * parse the syntax.
 	 *
@@ -91,42 +92,42 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 		$match = substr ( str_replace ( ' ', '', $match ), 12, - 2 );
 		$params = explode ( '&', $match );
 		list ( $img, $width, $sequences, $legs ) = $params;
-		
+
 		// make sure we have a min. width & sanity check
 		$width = intval ( $width );
 		if ($width < 100)
 			$width = 320;
 		if ($width > 2048)
 			$width = 2048;
-		
+
 		return array (
 				hsc ( $img ),
 				$width,
 				hsc ( $sequences ),
 				hsc ( $legs ),
-				$float 
+				$float
 		);
 	}
-	
+
 	/**
-	 * rendering the syntax.
-	 * 
+	 * render the syntax for xhtml, odt or metadata.
+	 *
 	 * @see DokuWiki_Syntax_Plugin::render()
 	 */
 	public function render($mode, Doku_Renderer &$renderer, $data) {
 		if ($data === false)
 			return false;
-		
+
 		static $id = 0;
 		list ( $image, $width, $sequences, $legs, $float ) = $data;
-		// this url might break, no idea if this url will be persistant but it 
+		// this url might break, no idea if this url will be persistant but it
 		// is mentioned in the api docs
 		$image_url = 'http://d1cuyjsrcm0gby.cloudfront.net/' . $image . '/thumb-1024.jpg';
-		
+
 		if ($mode == 'xhtml') {
 			// based on the embed javascript at http://www.mapillary.com/integrate.html
 			$height = ($width / 4 * 3 * 2 - 30);
-			$url = 'http://www.mapillary.com/jsapi/?';
+			$url = '//www.mapillary.com/jsapi/?';
 			if (! empty ( $image )) {
 				$url .= 'image=' . $image . '&';
 			}
@@ -136,14 +137,16 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 			if (! empty ( $legs )) {
 				$url .= 'legs=' . $legs;
 			}
-			
-			$renderer->doc .= '<div id="mapillary' . $id . '" class="mapillary-' . $float . '">';
-			$renderer->doc .= '<iframe src="' . $url . '" id="mapillary-iframe" style="width:' . $width . 'px;height:' . $height . 'px;" title="Mapillary (' . $image . ')">';
+
+			$renderer->doc .= '<div id="mapillary-' . $id . '" class="mapillary-' . $float . '">';
+			$renderer->doc .= '<iframe src="' . $url . '" id="mapillary-iframe-' . $id . '"';
+			$renderer->doc .= '  width="' . $width . '" height="' . $height . '" title="Mapillary (' . $image . ')">';
 			$renderer->doc .= '</iframe>';
-			$renderer->doc .= '<div class="mapillary-print">';
+			$renderer->doc .= '<figure class="mapillary-print">';
 			$renderer->externalmedia ( $image_url, 'Mapillary (' . $image . ')', 'left', 1024, null, 'cache', 'nolink' );
-			$renderer->externallink ( 'http://www.mapillary.com/map/im/' . $image, 'Mapillary (' . $image . ')' );
-			$renderer->doc .= '</div>';
+			$renderer->doc .= '<figcaption>Mapillary: ';
+			$renderer->externallink ( 'http://www.mapillary.com/map/im/' . $image, $image );
+			$renderer->doc .= ' (CC BY-SA 4.0)</figcaption></figure>';
 			$renderer->doc .= '</div>';
 			$id ++;
 			return true;
@@ -158,6 +161,10 @@ class syntax_plugin_mapillary extends DokuWiki_Syntax_Plugin {
 		} elseif ($mode == 'odt') {
 			$renderer->p_open ();
 			$renderer->externalmedia ( $image_url, 'Mapillary (' . $image . ')', 'left', 1024, null, 'cache', 'nolink' );
+			$renderer->linebreak();
+			$renderer->cdata('Mapillary: ');
+			$renderer->externallink ( 'http://www.mapillary.com/map/im/' . $image, $image );
+			$renderer->cdata(' (CC BY-SA 4.0)');
 			$renderer->p_close ();
 			return true;
 		}
